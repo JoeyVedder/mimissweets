2f3c7f7a322abb9ffaa3de247c27fa028f442b77
Thomas Stemler <stemlertho@gmail.com>
2025-06-14 13:36:32 -0500

    Formatted file

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 const HomePage: React.FC = () => {
-  return (
-    <div className="min-h-screen bg-pink-50 text-gray-800">
-      <section className="text-center py-20 px-4">
-        <h2 className="text-4xl font-extrabold text-pink-700 mb-4">
-          Satisfying Every Sweet Tooth 
-        </h2>
-      </section>
-    </div>
-  );
+    return (
+        <div className="min-h-screen bg-pink-50 text-gray-800">
+            <section className="text-center py-20 px-4">
+                <h2 className="text-4xl font-extrabold text-pink-700 mb-4">
+                    Satisfying Every Sweet Tooth
+                </h2>
+            </section>
+        </div>
+    );
 };
 
 export default HomePage;
