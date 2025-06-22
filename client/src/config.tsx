58f17d140f24daf2691d0f0976dc151f38655999
Thomas Stemler <stemlertho@gmail.com>
2025-06-22 16:55:50 -0500

    added routes to the configs

--- client/src/config.tsx
+++ client/src/config.tsx
@@ -1,24 +1,43 @@
 import HomePage from "./pages/Home.tsx";
 import AboutPage from "./pages/About.tsx";
-import type {IRoute} from "./types.ts";
+import type { IRoute } from "./types.ts";
+import ContactPage from "./pages/Contact.tsx";
+import OrderPage from "./pages/Order.tsx";
+import MenuPage from "./pages/Menu.tsx";
 
 /**
  * The routes configuration for the application.
  *
  * This is the list that defines the paths to different pages in the application
- * The first path will ***ALWAYS*** be the ***INDEX***. 
+ * The first path will ***ALWAYS*** be the ***INDEX***.
  * That means that the ***first route will always be the one initially served to the user***
  */
 export const routes: IRoute[] = [
-    {
-        path: "/",
-        name: "Home",
-        element: <HomePage/>
-    },
-    {
-        path: "/about",
-        name: "About The Baker",
-        element: <AboutPage />,
-        visible: true
-    }
+	{
+		path: "/",
+		name: "Home",
+		element: <HomePage/>,
+		visible: false
+	},
+	{
+		path: "/order",
+		name: "Order",
+		element: <OrderPage/>
+	},
+	{
+		path: "/menu",
+		name: "Menu",
+		element: <MenuPage/>
+	},
+	{
+		path: "/about",
+		name: "About The Baker",
+		element: <AboutPage/>,
+		visible: true
+	},
+	{
+		path: "/contact",
+		name: "Contact Us",
+		element: <ContactPage/>,
+	},
 ]
