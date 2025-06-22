837072aa041b019cb73a501eaf3341c4d18b33ab
Thomas Stemler <stemlertho@gmail.com>
2025-06-22 16:41:07 -0500

    404 error page done!

--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -1,8 +1,8 @@
 export default function ErrorPage() {
     return (
-        <div>
-            <h1>404 - Page Not Found</h1>
-            <p>The page you are looking for does not exist.</p>
+        <div className="flex flex-col items-center justify-center h-screen bg-pink-50 text-gray-800">
+          <h1 className="text-4xl font-extrabold text-pink-700 mb-4">404 Not Found</h1>
+          <p className="text-xl">The page you are looking for does not exist.</p>
         </div>
     )
 }
