42d4e5340bc9db33e775ea65c3c3492533016592
Thomas Stemler <stemlertho@gmail.com>
2025-06-22 16:55:40 -0500

    Added placeholder information for pages

---
+++ client/src/pages/Order.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const OrderPage: React.FC = () => {
+	return (
+		<div className="min-h-screen bg-pink-50 text-gray-800">
+			<section className="text-center py-20 px-4">
+				<h2 className="text-4xl font-extrabold text-pink-700 mb-4">
+					Order Page
+				</h2>
+			</section>
+		</div>
+	);
+};
+
+export default OrderPage;
